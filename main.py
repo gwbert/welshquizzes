5be44eb7b31412ebe622b451f5acdc7511678205
gwbert <64643035+gwbert@users.noreply.github.com>
2020-06-20 06:50:25 +0100

    commit

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.route("/index2")
+def index2():
+    return render_template('index2.html')
+
 @app.route("/culture")
 def culture():
     return render_template('culture.html')
@@ -35,6 +39,35 @@ def picturequiz(quizName):
     myObject = getattr(quizzes, quizName)
     return render_template('picturequiz.html', myObject=myObject)
 
+@app.route("/picturequiz2/<quizName>")
+def picturequiz2(quizName):
+    import quizzes
+    myObject = getattr(quizzes, quizName)
+    return render_template('picturequiz2.html', myObject=myObject)
+
+@app.route("/fivequiz/<quizName>")
+def fivequiz(quizName):
+    import quizzes
+    myObject = getattr(quizzes, quizName)
+    return render_template('fivequiz.html', myObject=myObject)
+
+@app.route("/tenquiz/<quizName>")
+def tenquiz(quizName):
+    import quizzes
+    myObject = getattr(quizzes, quizName)
+    return render_template('tenquiz.html', myObject=myObject)
+
+@app.route("/twentyquiz/<quizName>")
+def twentyquiz(quizName):
+    import quizzes
+    myObject = getattr(quizzes, quizName)
+    return render_template('twentyquiz.html', myObject=myObject)
+
+@app.route("/thirtyquiz/<quizName>")
+def thirtyquiz(quizName):
+    import quizzes
+    myObject = getattr(quizzes, quizName)
+    return render_template('thirtyquiz.html', myObject=myObject)
 
 application = Flask(__name__)
 
